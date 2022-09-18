5c163b59e56805be2914eaaacd85aee108034cf6
Biraj Rai <70630924+birajrai@users.noreply.github.com>
2022-09-18 02:57:01 +0000

    👾: Final Commit for CDN

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 const path = require('path')
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
